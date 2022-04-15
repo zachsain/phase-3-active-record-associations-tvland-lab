2466f02853892a299871321b3b1e666971b5379d
Zachary Sain <zachsain@gmail.com>
2022-04-15 13:29:03 -0500

    completed

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,9 +5,16 @@ class Show < ActiveRecord::Base
 
     def actors_list
         #want to be able to use a show to show us the all of the actors full name 
-        self.characters.map do |n|
-            n.actors.first_name
+        array = []
+        self.actors.map do |n|
+            
+           first =  n.first_name
+           last = n.last_name
+           full_name = "#{first} #{last}"
+           array << full_name
+         
         end 
+        array
     end 
   
 end
